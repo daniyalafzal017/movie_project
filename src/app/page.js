b2d2a9db0e12973e157ef0aa1dc6a520aea77486
Daniyal Afzal <114574250+DaniyalAfzalDev@users.noreply.github.com>
2025-04-02 16:31:29 +0500

    all set

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,22 @@
-import Image from "next/image";
+"use client";
+import dynamic from 'next/dynamic';
 import styles from "./page.module.css";
-import MovieSlider from "./components/Slider/MovieSlider";
+
+// Dynamically import components with ssr: false to disable SSR
+const MovieSlider = dynamic(() => import("./components/Slider/MovieSlider"), { ssr: false });
+const CardSlider = dynamic(() => import("./components/CardSlider/CardSlider"), { ssr: false });
+const MostWatched = dynamic(() => import("./components/MostWatched/MostWatched"), { ssr: false });
+const TrendingMovies = dynamic(() => import("./components/TrendingMovies/TrendingMovies"), { ssr: false });
+
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-       <MovieSlider />
+        <MovieSlider />
+        <CardSlider />
+        <MostWatched />
+        <TrendingMovies />
       </main>
-    
     </div>
   );
 }
